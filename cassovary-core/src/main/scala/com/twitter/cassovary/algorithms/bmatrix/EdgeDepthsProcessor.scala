5183378dc431de16083132b0ba622118b32d8100
Wojtek Mielczarek <wojtek.mielczarek@gmail.com>
2015-11-12 12:52:12 +0100

    Newlines and comments fixed.

--- cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/EdgeDepthsProcessor.scala
+++ cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/EdgeDepthsProcessor.scala
@@ -8,17 +8,13 @@ private class EdgeDepthsProcessor(edgeBMatrix: BMatrix, graph: DirectedGraph[Nod
 
   def processDepths(nodeId: Int, depths: collection.Map[Int, Int]) = {
     graph.foreach(node => {
-      //printf("*** %d, %d, %s \n", nodeId, node.id, node.neighborIds(GraphDir.OutDir).toString())
-      node.neighborIds(GraphDir.OutDir).toSet.foreach({ neighborId:Int => {
+      node.neighborIds(GraphDir.OutDir).toSet.foreach({ neighborId: Int => {
         if (node.id < neighborId && depths.contains(neighborId)) {
           val depth = depths(node.id) + depths(neighborId)
           if (depth > 0) {
             incrementForDepth(depth)
           }
         }
-        //else {
-        //  printf("SOURCE: %d, NODE id: %d, MISSING KEY: %d\n", nodeId, node.id, neighborId)
-        //}
       }
       })
     })
