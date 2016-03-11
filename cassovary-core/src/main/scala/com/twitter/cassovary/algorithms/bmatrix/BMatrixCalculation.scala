d44cbf34b1e70ab09e7c78cf00258d74c2220bac
Wojtek Mielczarek <wojtek.mielczarek@gmail.com>
2016-03-11 18:15:46 +0100

    Bug fixing.

--- cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/BMatrixCalculation.scala
+++ cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/BMatrixCalculation.scala
@@ -51,16 +51,16 @@ private class BfsTask(parallelExecutionContext: ParallelExecutionContext,
       val vertexBMatrix = data(0)
       val edgeBMatrix = data(1)
 
-      val vertexDepthProcessor = new VertexDepthsProcessor(vertexBMatrix)
-      val edgeDepthProcessor = new EdgeDepthsProcessor(edgeBMatrix)
-
-
       nodes.foreach { node =>
         val bfs = new BreadthFirstTraverser(graph, GraphDir.OutDir, Seq(node.id), Walk.Limits())
         //We traverse the graph to get depths
         bfs.foreach(_ => {})
 
         val depths = bfs.depthAllNodes()
+
+        val vertexDepthProcessor = new VertexDepthsProcessor(vertexBMatrix)
+        val edgeDepthProcessor = new EdgeDepthsProcessor(edgeBMatrix)
+
         vertexDepthProcessor.processDepths(node.id, depths)
         edgeDepthProcessor.processDepths(node.id, depths, graph, undirectedFlag)
 
@@ -72,7 +72,7 @@ private class BfsTask(parallelExecutionContext: ParallelExecutionContext,
 
         progress.inc
       }
-      println("Finished calculation for nodes " + nodes.map(x => x.id).mkString(","))
+      printf("Finished calculation in %s for nodes %s\n", executorThreadName, nodes.map(x => x.id).mkString(","))
     } catch {
       case e: Exception => {
         println(e)
@@ -139,7 +139,7 @@ private class BMatrixCalculation(graph: DirectedGraph[Node]) {
     var processedNodes = 0
 
     var nodes = List[Node]()
-    val multitaskLimit = 20
+    val multitaskLimit = 10
     graph.foreach { node =>
       if (processedNodes >= currentPartitionFrom && processedNodes < currentPartitionTo) {
         if (nodes.size < multitaskLimit) {
