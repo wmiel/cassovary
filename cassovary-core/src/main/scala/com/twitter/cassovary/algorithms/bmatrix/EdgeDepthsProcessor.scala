0a1f9f559b6d98af6d72be60f0c8ef733681c1d5
Wojtek Mielczarek <wojtek.mielczarek@gmail.com>
2016-03-16 01:21:23 +0100

    Removing toSet

--- cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/EdgeDepthsProcessor.scala
+++ cassovary-core/src/main/scala/com/twitter/cassovary/algorithms/bmatrix/EdgeDepthsProcessor.scala
@@ -7,12 +7,12 @@ private class EdgeDepthsProcessor(matrix: HashBasedSparseMatrix) extends DepthsP
   var bmatrix = matrix
 
   def processDepths(nodeId: Int, depths: collection.Map[Int, Int], graph: DirectedGraph[Node], undirectedFlag: Boolean) = {
-    graph.foreach(node => {
+    depths.keys.foreach(nodeId => {
+      val node = graph.getNodeById(nodeId).get
+      //var checkedNodes = scala.collection.mutable.Set[Int]()
       node.neighborIds(GraphDir.OutDir).foreach({ neighborId: Int => {
-        //        if (depths.contains(neighborId) && depths.contains(node.id)) {
-        val nodeId = node.id
         if (((nodeId < neighborId && undirectedFlag) || (!undirectedFlag))
-          && depths.contains(neighborId) && depths.contains(nodeId)) {
+          && depths.contains(neighborId)) {
           val depth = depths(nodeId) + depths(neighborId)
           if (depth > 0) {
             incrementForDepth(depth)
